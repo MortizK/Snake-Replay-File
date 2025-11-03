eb7035a93fb9346c59e4217a807c297965118e2c
Moritz Köhler <moritz-koehler@freenet.de>
2025-11-03 14:39:10 +0100

    findStartOfSegments is functional

--- ReplayHandler.py
+++ ReplayHandler.py
@@ -34,6 +34,9 @@ class ReplayHandler:
     | For each segment:
         - packed move bytes                               -> 2 bits * moves + 2 bits
     """
+
+    HEADER_SIZE_WITHOUT_INITIAL_SNAKE = 13
+    MAPPING = {'S': 0b00, 'R': 0b01, 'L': 0b10}
     
     def encode_moves_bitpacked(self, moves: str, lastbyte: bytes) -> bytes:
         """
@@ -48,7 +51,6 @@ class ReplayHandler:
             11 -> End of Segment
         """
         
-        mapping = {'S': 0b00, 'R': 0b01, 'L': 0b10}
         bits = 0
         bit_len = 0
 
@@ -62,7 +64,7 @@ class ReplayHandler:
             mask = mask << 2
 
         for move in moves:
-            bits = (bits << 2) | mapping[move]
+            bits = (bits << 2) | self.MAPPING[move]
             bit_len += 2
 
         # Add 11 (end of segment marker)
@@ -98,15 +100,15 @@ class ReplayHandler:
         height = meta["map"]["height"]
         binary_data.extend(struct.pack("BB", width, height))
 
+        # Seed (4 bytes)
+        seed = meta["seed"]
+        binary_data.extend(struct.pack("I", seed))
+
         # Initial snake (no direction)
         snake = meta["initial"]["snake"]
         binary_data.extend(struct.pack("B", len(snake)))
         for pos in snake:
             binary_data.extend(struct.pack("H", pos))
-
-        # Seed (4 bytes)
-        seed = meta["seed"]
-        binary_data.extend(struct.pack("I", seed))
 
         last_seg_byte = 0b0
         binary_data.extend(struct.pack("B", last_seg_byte))
@@ -127,7 +129,7 @@ class ReplayHandler:
             data = f.read()
 
         offset = 0
-        header = data[offset:offset + 4]
+        header = data[:offset + 4]
         offset += 4
         if header != b"SNAK":
             raise ValueError("Invalid file format")
@@ -142,15 +144,15 @@ class ReplayHandler:
         width, height = struct.unpack_from("BB", data, offset)
         offset += 2
 
+        # Seed
+        seed, = struct.unpack_from("I", data, offset)
+        offset += 4
+
         # Snake
         snake_len, = struct.unpack_from("B", data, offset)
         offset += 1
         snake = [struct.unpack_from("H", data, offset + i*2)[0] for i in range(snake_len)]
         offset += snake_len * 2
-
-        # Seed
-        seed = struct.unpack_from("I", data, offset)
-        offset += 4
 
         # Segments
         lenData = len(data)
@@ -185,12 +187,24 @@ class ReplayHandler:
     def updateResult(self, filepath: str, score: int, reason: int = 1) -> bytes:
         with open(filepath, "r+b") as f:
             # Score at byte offset 4
-            f.seek(4)
+            f.seek(4)  # "SNAK" is 4 bytes
             f.write(struct.pack("H", score))  # New Score
 
             # Reason at byte offset 6
-            f.seek(6)
+            f.seek(6)  # "SNAK" + score is 6 bytes
             f.write(struct.pack("B", reason))     # New Reason
+
+    def findStartOfSegments(self, filepath: str) -> int:
+        '''
+        Returns the size in bytes of the entire Header with the initial snake
+        '''
+        with open(filepath, "rb") as f:
+            f.seek(self.HEADER_SIZE_WITHOUT_INITIAL_SNAKE)
+            snake_len_data = f.read(1)
+            snake_len, = struct.unpack("B", snake_len_data)
+
+            offset = self.HEADER_SIZE_WITHOUT_INITIAL_SNAKE + snake_len * 2 + 1
+            return offset
 
     def addSegments(self, filepath: str, segments: list[str]) -> bytes:
         with open(filepath, "r+b") as f:
@@ -218,8 +232,15 @@ if __name__ == "__main__":
     handler = ReplayHandler()
     path = "replay.bin"
 
-    # ReplayHandler().addSegments(path, ["L", "RR", "SLS"])
-    # ReplayHandler().updateResult(path, 42, 2)
+    # read json input
+    # with open(path + ".json", "r") as f:
+    #     input_data = json.load(f)
+
+    # handler.encode_to_binary(input_data, path)
+
+    # handler.addSegments(path, ["L", "RR", "SLS"])
+    # handler.updateResult(path, 42, 2)
+    handler.findStartOfSegments(path)
     
     # Load externally (for example purposes)
     # path = input("Enter replay file (.bin): ").strip()
